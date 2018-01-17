f54bf6cb6e820f713d6f25628435d26373fa53d0
Gaston Cuervo <cuervogaston@gmail.com>
2018-01-17 20:48:07 +0000

    Pom update and CallCenterException deleted

--- src/main/java/com/gcuervo/callcenter/exceptions/CallCenterException.java
+++
@@ -1,9 +0,0 @@
-package com.gcuervo.callcenter.exceptions;
-
-public class CallCenterException extends Exception {
-  private String NO_EMPLOYEE_AVAILABLE = "No employee available at the moment";
-
-  public CallCenterException(String msj) {
-    super(msj);
-  }
-}
